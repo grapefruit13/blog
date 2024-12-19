93f7d9c863d1971d85f178279c88a21c0235aab4
grapefruit13 <liam.heo11@gmail.com>
2024-12-19 14:33:39 +0900

    refactor: add syntax

--- src/app/_components/theme-switcher.tsx
+++ src/app/_components/theme-switcher.tsx
@@ -8,14 +8,13 @@ const modes = ["system", "dark", "light"] as const;
 type ColorSchemePreference = (typeof modes)[number];
 
 const ThemeSwitcher = () => {
-  const [mode, setMode] = useState<ColorSchemePreference>(() => {
-    return (
+  // theme mode state
+  const [mode, setMode] = useState<ColorSchemePreference>(
+    () =>
       ((typeof localStorage !== "undefined" &&
         localStorage.getItem(STORAGE_KEY)) as ColorSchemePreference) ?? "system"
-    );
-  });
+  );
 
-  // 클라이언트에서 DOM 업데이트를 위한 useEffect
   // updating DOM on Client side with useEffect
   useEffect(() => {
     const savedMode = localStorage.getItem(STORAGE_KEY) ?? "system";
@@ -24,14 +23,12 @@ const ThemeSwitcher = () => {
       : "light";
     const resolvedMode = savedMode === "system" ? systemMode : savedMode;
 
-    // 클라이언트에서만 className과 data-mode 업데이트
-    // updating className and data-mode only in Client side
+    // updating className and data-mode only on Client side
     document.documentElement.classList.add(resolvedMode);
     document.documentElement.setAttribute("data-mode", savedMode);
     localStorage.setItem(STORAGE_KEY, savedMode);
   }, []);
 
-  // 모드 변경 처리
   /** when switch mode **/
   useEffect(() => {
     localStorage.setItem(STORAGE_KEY, mode);
